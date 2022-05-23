84288b06190477d83ded3904a2c75dacef36c42f
itzyashh <yashjadhav1411@rocketmail.com>
2022-05-24 00:00:48 +0530

    3 products added post req working

---
+++ pages/api/products/index.js
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dbConnect from "../../../util/mongo"
+import Product from "../../../models/Product";
+
+export default async function handler(req, res) {
+    const { method } = req;
+
+    dbConnect()
+
+    if (method === "GET") { }
+
+
+    if (method === "POST") {
+        try {
+            const product = await Product.create(req.body)
+            res.status(201).json(product)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    }
+}
