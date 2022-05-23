40913f559c1f0859f6248ff5c3930e96789c7a5f
itzyashh <yashjadhav1411@rocketmail.com>
2022-05-24 00:25:14 +0530

    GET working + Dynamic +Api
    GET working + dynamic component added using api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
 import Featured from "../components/Featured";
 import PizzaList from "../components/PizzaList";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({pizzaList}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,8 +13,19 @@ export default function Home() {
         <meta name="description" content="Best pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Featured/>
-      <PizzaList/>
+      <Featured />
+      <PizzaList pizzaList={pizzaList}/>
     </div>
   );
+}
+
+export const getServerSideProps = async () => {
+  const res = await axios.get("http://localhost:3000/api/products")
+  return {
+    props:{
+      pizzaList: res.data,
+
+    }
+    
+  }
 }
